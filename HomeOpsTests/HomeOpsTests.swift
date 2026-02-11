ad57a5dffd9c141d5f7e7e70eed77b0c4b3add1d
kowshik24 <kowshikcseruet1998@gmail.com>
2026-02-11 21:05:18 +0600

    Initial Commit

---
+++ HomeOpsTests/HomeOpsTests.swift
@@ -0,0 +1,16 @@
+//
+//  HomeOpsTests.swift
+//  HomeOpsTests
+//
+//  Created by Kowshik on 11/2/26.
+//
+
+import Testing
+
+struct HomeOpsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
